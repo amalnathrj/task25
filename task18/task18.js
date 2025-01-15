4350efd622b4d2e48c44a271a01cd4953ff75900
Amal Nath RJ <amalnathrj@gmail.com>
2025-01-15 10:12:33 +0530

    first commit

---
+++ task18/task18.js
@@ -0,0 +1,81 @@
+// 1)let numbers = [10, undefined, 90...];
+
+// Write a  loop to iterate through the array and log only the numbers to the console, skipping any undefined values.
+
+
+
+// let numbers = [10, undefined, 90,100,200]
+
+// for(number of numbers){
+//     if(number == undefined){
+//         continue
+//     }else{
+//         console.log(number);
+        
+//     }
+// }
+
+
+// const fruits = {
+//     fruit1: "apple",
+//     ...
+// };
+// Write a loop to iterate over the object and log only the properties whose values start with a specific letter 
+
+// const fruits = {
+//     fruit1: "apple",
+//     fruit2: "orange",
+//     fruit3: "grapes",
+//     fruit4: "papaya"
+
+// }
+
+// for(let fruit in fruits){
+//     let fruiiit =fruits[fruit];
+//     if(fruiiit.startsWith('a')){
+//         console.log(fruiiit);
+        
+//     }
+  
+// }
+
+// WAP to find the count of a given number in an array
+
+// let numbers = [20,30,20,50,50]
+
+//  let sumofnum = numbers.reduce((a,b)=> a+b)
+//  console.log(sumofnum);
+
+// Write a program to reverse an array
+
+// let numbers = [20,30,20,50,50]
+
+// let reverseArray = numbers.reverse()
+// console.log(reverseArray);
+
+// Write a program to remove all negative numbers from an array
+
+// let numbers = [20,30,20,50,50,-1]
+
+// negativeNum = numbers.filter((a)=> a>0)
+
+// console.log(negativeNum);
+
+// Write a program to concatenate two array
+
+// let numbers = [20,30,20,50,50]
+
+// let numb = [20,30,20,50,50]
+
+// let conacatArray = numbers.concat(numb)
+
+// console.log(conacatArray);
+
+
+
+
+
+
+ 
+
+
